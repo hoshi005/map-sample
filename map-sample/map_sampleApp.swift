68688a104007697937e437ab0c31613a259a8d53
Susumu Hoshikawa <hoshi005@me.com>
2020-12-01 20:07:41 +0900

    first commit

---
+++ map-sample/map_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  map_sampleApp.swift
+//  map-sample
+//
+//  Created by Susumu Hoshikawa on 2020/12/01.
+//
+
+import SwiftUI
+
+@main
+struct map_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
