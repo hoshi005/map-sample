372662408e46be0f57211dc1ae6fee7f1983a054
Susumu Hoshikawa <hoshi005@me.com>
2020-12-06 10:43:13 +0900

    feat: MapAnnotationViewにNamespaceを渡せるように

--- map-sample/MapAnnotationView.swift
+++ map-sample/MapAnnotationView.swift
@@ -10,12 +10,14 @@ import SwiftUI
 struct MapAnnotationView: View {
     
     var item: MapItem
+    var namespace: Namespace.ID
     
     var body: some View {
         
         Image(systemName: "bubble.middle.bottom.fill")
             .resizable()
             .aspectRatio(contentMode: .fit)
+            .matchedGeometryEffect(id: item.id, in: namespace)
             .frame(width: 44.0)
             .foregroundColor(item.color)
             .shadow(color: Color.black.opacity(0.5), radius: 5, x: 5, y: 5)
@@ -23,8 +25,11 @@ struct MapAnnotationView: View {
 }
 
 struct MapAnnotationView_Previews: PreviewProvider {
+    
+    @Namespace static var namespace
+    
     static var previews: some View {
-        MapAnnotationView(item: MapItem())
+        MapAnnotationView(item: MapItem(), namespace: namespace)
             .previewLayout(.fixed(width: 200.0, height: 200.0))
     }
 }
